c4cd4bbbc0a6d54ad79656f5d639450b88a06150
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-23 19:16:53 +0700

    useEffect and useContext

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,16 +16,48 @@
 //     }
 // }
 
+// import React from 'react'
+// export default function Navigation(){
+//     return(
+//         <nav>
+//              <ul className='navigation-ul'>
+//                 <li><a className='active' href='#home'>Home</a></li>
+//                 <li><a href='#news'>News</a></li>
+//                 <li><a href='#about'>About</a></li>
+//                 <li><a href='#contact'>Contact</a></li>
+//              </ul>
+//         </nav>
+//     )
+// }
+
+
 import React from 'react'
-export default function Navigation(){
-    return(
-        <nav>
-             <ul className='navigation-ul'>
-                <li><a className='active' href='#home'>Home</a></li>
-                <li><a href='#news'>News</a></li>
-                <li><a href='#about'>About</a></li>
-                <li><a href='#contact'>Contact</a></li>
-             </ul>
-        </nav>
-    )
+import { useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
+export default function Navigation() {
+  const { theme, toggle, dark } = useContext(ThemeContext)
+  return (
+    <div>
+      <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+          <ul className='navigation-ul'>
+              <li><a className='active' href='#home'>Home</a></li>
+              <li><a href='#news'>News</a></li>
+              <li><a href='#about'>About</a></li>
+              <li><a href='#contact'>Contact</a></li>
+</ul>
+          <div style={{position: 'relative'}}>
+          <a className='switch-mode' href='#' onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: 'none'
+          }} data-testid="toggle-theme-btn"
+        >
+          Switch Nav to {!dark ? 'Dark' : 'Light'} mode
+         </a>
+         </div>
+      </nav>
+      </div>
+  )
 }
+
